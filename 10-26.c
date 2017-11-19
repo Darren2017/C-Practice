fa6975ffd01625c0cb4bfcec90e35ed130d4704f
Darren2017 <33794339+Darren2017@users.noreply.github.com>
2017-11-19 08:36:29 +0800

    Create 10-26.c

---
+++ 10-26.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+main()
+{
+	const int MINOR=35;
+	
+	int age=0;
+	
+	printf("请输入您的年龄：");
+	scanf("%d",&age);
+	printf("您的年龄是：%d\n",age);
+	 
+	if(age<MINOR){
+		printf("年轻是美好的"); 
+	}
+	if(age>=MINOR){
+		printf("年龄决定了你的精神世界，好好珍惜吧。\n");
+	}
+	getch();
+	return 0;
+ } 
