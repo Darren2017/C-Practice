eef4b9772be5237ead232a1723a7f14c7faa8641
Darren2017 <17362990052@163.com>
2017-11-25 00:22:29 +0800

    s

---
+++ 11-2408.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,j;
+    int num;            /*实际学生人数*/  
+    int gola[80];   /*各学生分数*/
+    int line[11]={0};   /*分布图各行数的人数*/
+    puts("请输入学生人数：");
+    do{
+        scanf("%d",&num);
+        if(num<=0||num>80){
+                            printf("请输入1～80的数");   
+        }
+    }while(num<=0||num>80);
+    for(i=0;i<num;i++){
+                        printf("%2d号：",i+1);
+                        do{
+                            scanf("%d",&gola[i]);   
+                            if(gola[i]<0||gola[i]>100){
+                                                    printf("请输入0～100之间的数值");
+                            }
+                        }while(gola[i]<0||gola[i]>100);
+                        line[gola[i]/10]++;
+    }
+    puts("分布图---------");
+    printf("    100:");
+    for(i=0;i<line[10];i++){puts("*");}
+    puts("");
+    for(j=9;j>=0;j--){
+                    printf("%3d~%3d:",j*10,j*10+9);
+                    for(i=0;i<line[j];i++){putchar('*');}
+                    puts("");
+    }
+return 0;
+}
