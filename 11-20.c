58ed5353fe8b8a506d59d58362a58f854ea2fb3d
Darren2017 <17362990052@163.com>
2017-11-26 15:16:06 +0800

    s

--- 11-20.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>>
-
-int main()
-{
-	puts("tian");
-	
-ruturn 0;
-}
---
+++ 11-2501.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+#define NUMBER 80
+
+int main()
+{
+    int i,j;
+    int num;            /*实际学生人数*/  
+    int gola[NUMBER];   /*各学生分数*/
+    int line[11]={0};   /*分布图各行数的人数*/
+    puts("请输入学生人数：");
+    do{
+        scanf("%d",&num);
+        if(num<=0||num>80){
+                            printf("请输入1～%d的数",NUMBER);   
+        }
+    }while(num<=0||num>80);
+    
+    for(i=0;i<num;i++){
+        printf("%2d号：",i+1);
+        do{
+            scanf("%d",&gola[i]);   
+            if(gola[i]<0||gola[i]>100){
+                                    printf("请输入0～100之间的数值");
+            }
+        }while(gola[i]<0||gola[i]>100);
+        line[gola[i]/10]++;
+    }
+    puts("分布图---------");     /*正序输出成绩*/
+    for(j=0;j<10;j++){
+            printf("%3d~%3d:",j*10,j*10+9);
+            for(i=0;i<line[j];i++){putchar('*');}
+            puts("");
+    }
+    printf("    100:");
+    for(i=0;i<line[10];i++){puts("*");}
+    puts("");
+    
+    return 0;
+}
