995afdfb37f995fc0c8340a34ce53d959c7bd8e8
github <github>
2017-12-08 16:29:10 +0800

    s

---
+++ 12-0803.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+void put_atringr(const char a[])
+{
+    int i=0;
+    while(a[i]){
+        i++;
+    }
+    while(i){
+        putchar(a[i-1]);
+        i--;
+    }
+}
+
+int main()
+{
+    char str[100];
+    printf("请输入字符串：\n");
+    scanf("%s",str);
+
+    printf("字符串的逆向输出是：");
+    put_atringr(str);
+    
+    putchar('\n');
+
+    return 0;
+}
