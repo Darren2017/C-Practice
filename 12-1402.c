a5bbdbf2e8996df3f3f26474dfd8d5133349740b
github <github>
2017-12-14 22:00:26 +0800

    s

--- 12-1402.c
+++ 12-1402.c
@@ -13,7 +13,7 @@ int main()
     int i;
     int a[] = {1,2,3,4,5};
 
-    ary_set(&a[2], 2, 99);
+    ary_set(&a[2], 2, 99);  /*a[2]被当作数组的第一个元素传入ary_set函数*/
     
     for(i =0; i < 5; i++){
         printf("a[%d] = %d\n", i, a[i]);
